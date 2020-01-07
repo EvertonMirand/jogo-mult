b9f8129e78678116f7829bf238aa3ac7398dcff6
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-01-07 20:45:21 -0300

    Me veja programar um Backend que troca informação em tempo-real

--- playground/1st-release/server.js
+++ playground/1st-release/server.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import http from 'http';
+import socketio from 'socket.io';
+
+import createGame from './public/game.js';
 
 const app = express();
-const serve = http.createServer(app);
+const server = http.createServer(app);
+
+const sockets = socketio(server);
 
 app.use(express.static('public'));
 
-serve.listen(3000, () => {
+const game = createGame();
+
+sockets.on('connection', socket => {
+  const player = socket.id;
+  console.log(`Player connect on server with id ${player}`);
+  socket.emit('setup', game.state);
+});
+
+server.listen(3000, () => {
   console.log('> Serve listening on port: 3000');
 });
